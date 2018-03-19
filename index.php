8b418ee965ed645ead1ba38c260ad31d96abe474
Sebastian Rincón Perez Linux <desarrollador@kubbox.com>
2018-03-18 21:43:21 -0500

    base url and index update version

--- index.php
+++ index.php
@@ -5,5 +5,5 @@
  * @author Brian Vanegas Parra @braian125
  * @link https://github.com/braian125/s3-skeleton
  */
-require_once __DIR__ . '/../bootstrap/app.php';
+require_once __DIR__ . '/bootstrap/app.php';
 $app->run();
